8080d76a1d9301d4de51f7000e8b06dbdd342f8e
Quake11 <winner435@gmail.com>
2019-09-24 17:33:55 +0300

    Fix flights

--- src/app/services/flights/flights.service.ts
+++ src/app/services/flights/flights.service.ts
@@ -18,17 +18,24 @@ export class FlightsService {
 
   private get _flights$(): Observable<Flight[]> {
     const apiKey = 'f44b5477-b410-40b1-b8b0-e7b9c3c95a8f&station=s9623562';
-    const date = new Date().toISOString();
     return this.http
       .get<{ schedule: Flight[] }>(
-        `https://api.rasp.yandex.net/v3.0/schedule/?apikey=${apiKey}&lang=ru_RU&transport_types=plane&date=${date}`
+        `https://api.rasp.yandex.net/v3.0/schedule/?apikey=${apiKey}&lang=ru_RU&transport_types=plane`
       )
       .pipe(
         map(({ schedule }) =>
-          schedule.map(s => {
-            s.thread.title = s.thread.title.split('—')[1].trim();
-            return s;
-          })
+          schedule
+            .map(s => {
+              s.thread.title = s.thread.title.split('—')[1].trim();
+              return s;
+            })
+            .filter(s => {
+              return (
+                new Date('01/01/2000 ' + s.departure) >
+                new Date('01/01/2000 ' + new Date().toTimeString())
+              );
+            })
+            .slice(0, 4)
         )
       );
   }
